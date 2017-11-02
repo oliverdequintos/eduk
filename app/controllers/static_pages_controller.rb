2692cab39c44c1a3ca15b584ed1464d9c492e3ad
Oliver <oliverdequintos@yahoo.com>
2017-11-02 10:42:20 +0800

    added dashboard info

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
+    @student_subjects = []
+    @student_subjects = StudentSubject.where(user_id: current_user.id) unless current_user.is_teacher?
+    @teacher_subjects = []
+    @teacher_subjects = TeacherSubject.where(user_id: current_user.id) if current_user.is_teacher?
+    @teacher_ratings = Rating.teacher_ratings(current_user.id)
   end
 end
